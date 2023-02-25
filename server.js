881ea6b26511291805651dbff19bcc3ebf3596d6
kim-0617 <kkk5993@naver.com>
2023-02-25 16:50:04 +0900

    :memo: server.js 완성 영화 목록과 id에 따른 영화 가져오는 api

---
+++ server.js
@@ -0,0 +1,67 @@
+import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
+
+const typeDefs = gql`
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+  }
+
+  type Query {
+    allMovies: [Movie!]!
+    movie(id: String!): Movie!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allMovies: async () => {
+      const res = await fetch(`https://yts.mx/api/v2/list_movies.json`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const ret = await res.json();
+      return ret.data.movies;
+    },
+
+    movie: async (_, { id }) => {
+      const res = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const ret = await res.json();
+      console.log(ret);
+      return ret.data.movie;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
